b39a235da0c0f040eb74ba0d3b2d0cafc56460c5
Lukox <filip.luk.2003@gmail.com>
2022-11-24 22:13:27 +0100

    Added Ball

--- index.js
+++ index.js
@@ -32,11 +32,45 @@ class Player{
     }
 }
 
+class Ball{
+    constructor(position){
+        this.position = position;
+        this.vel = {
+            x: 0,
+            y: 1,
+        }
+        this.height = 25;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.position.x, this.position.y, 25, 0, 2 * Math.PI);
+        c.fillStyle = "green";
+        c.fill();
+        c.stroke();
+      }
+
+    update(){
+        this.draw();
+        this.position.x += this.vel.x;
+        this.position.y += this.vel.y;
+        if(this.position.y +this.height+ this.vel.y< canvas.height)
+            this.vel.y += gravity;
+        else this.vel.y = 0;
+    }
+}
+
 
 const player = new Player({
     x: 100,
     y: 100,
 });
+
+const ball = new Ball({
+    x:300,
+    y: 100,
+});
+
 
 const keys = {
     ArrowRight: {
@@ -52,11 +86,17 @@ function animate(){
     c.fillStyle = 'white';
     c.fillRect(0,0,canvas.width,canvas.height);   
     player.update(); 
-    player.vel.x = 0.5;
-    if(keys.ArrowRight.pressed)
-        player.vel.x = 5;
-    else if (keys.ArrowLeft.pressed)
-        player.vel.x = -5;
+    ball.update();
+    player.vel.x = 0;
+    if(keys.ArrowRight.pressed){
+        if(player.position.x >= -player.height && player.position.x <= canvas.width - player.height){
+            player.vel.x = 5;
+        }
+    } else if (keys.ArrowLeft.pressed){
+        if(player.position.x >= 0 && player.position.x <= canvas.width){
+            player.vel.x = -5;
+        }
+    }
 }
 
 animate();
@@ -70,8 +110,10 @@ window.addEventListener('keydown', (event) =>{
         case 'ArrowLeft':
             keys.ArrowLeft.pressed = true;
             break;
-        case ' ':
-            player.vel.y = -15;
+        case 'ArrowUp':
+            if(player.position.y == (canvas.height - player.height)){
+                player.vel.y = -17;
+            } 
             break;
     }
 })
